69076ce0957359323dd3b31e294b60acd652c69d
Julia Nader <julianader4@gmail.com>
2022-08-31 00:07:48 +0200

    Adding Multiple Tags (Emails)

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const input = document.getElementById('myInput');
 
 let tags=[];
 
+input.addEventListener('keyup', function(e){
+    if (e.key === 'Enter'){
+        tags.push(input.value);
+        addTags();
+        input.value='';
+    }
+})
+
+function addTags(){
+    reset();
+    tags.slice().reverse().forEach(function(tag){
+        const input = createTag(tag);
+        tagContainer.prepend(input);
+    })
+}
+  
