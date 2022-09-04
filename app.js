b2662f2064862704e4370b20d718ae8e60170224
Julia Nader <julianader4@gmail.com>
2022-09-04 13:17:32 +0200

    Some Edits

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const tagContainer = document.getElementById('tag-container');
 const input = document.getElementById('myInput');
 
-let tags=[];
+let tags=[]; //this array represents different emails
 
-input.addEventListener('keyup', function(e){
-    if (e.key === 'Enter'){
+input.addEventListener('keyup', (e) => { //the keyup event when a key is clicked
+    if (e.key === 'Enter'){ //event
         tags.push(input.value);
-        addTags();
+        console.log(input.value);
+        addTags(); 
         input.value='';
     }
-})
+});
 
-function addTags(){
-    reset();
+const addTags = () => {
+    reset(); //reset the input value to enter another one
     tags.slice().reverse().forEach(function(tag){
-        const input = createTag(tag);
-        tagContainer.prepend(input);
+        const input = createTag(tag); //creates the tag shape
+        tagContainer.append(input); //to put the input in the last index of the array (in the tag container)
     })
-}
+};
   
 function reset(){
     document.querySelectorAll('.tag').forEach(function(tag){
@@ -26,13 +27,13 @@ function reset(){
 }
 
 function createTag(label){
-    const div = document.createElement('div');
+    const div = document.createElement('div'); //for the container
     div.setAttribute('class', 'tag');
-    const span = document.createElement('span')
+    const span = document.createElement('span') //for the text aera
     span.innerHTML = label;
-    const closeBtn = document.createElement('i');
+    const closeBtn = document.createElement('i'); //for the 'x' mark
     closeBtn.setAttribute('class', 'material-icons');
-    closeBtn.setAttribute('data-item', label);
+    closeBtn.setAttribute('data', label);
     closeBtn.innerHTML='x';
     
     div.appendChild(span);
@@ -41,10 +42,10 @@ function createTag(label){
 }
 
 document.addEventListener('click', function(e) {
-    if (e.target.tagName === 'I') {
-        const value= e.target.getAttribute('data-item');
+    if (e.target.tagName === 'i') {
+        const value= e.target.getAttribute('data');
         const index= tags.indexOf(value);
-        tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+        tags = [...tags.slice(0, index), ...tags.slice(index + 1)]; //To remove the unwanted email 
         addTags();
     }
 })
@@ -69,7 +70,7 @@ function send(){
 }
 
 function validateEmail(email){
-    const re=  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const re=  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; //REGEX
     return re.test(email);
 }
 function sendEmail(){
