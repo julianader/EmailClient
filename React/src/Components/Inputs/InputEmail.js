739c1e4b1a5eb4820b52ce2b41c1f5e51cef2950
Bichoy Messiha <bichoy.messiha@indigen.com>
2022-09-17 14:35:14 +0200

    move button and state to the form

--- React/src/Components/Inputs/InputEmail.js
+++ React/src/Components/Inputs/InputEmail.js
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { Tag } from "../Core/Tag";
 
-export const InputEmail = props => {
-    const [emails, setEmails] = useState([]);
-
+export const InputEmail = ({ emails, setEmails }) => {
     const [inputValue, setInputValue] = useState("");
 
     return (
@@ -25,53 +23,33 @@ export const InputEmail = props => {
                 onKeyUp={e => {
                     if (e.key === "Enter") {
                         // TODO Before adding this email, it should be valid
-                        const re=  /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; //REGEX
-                        if (re.test(inputValue) === true){
-                            document.getElementById('added').style.display = "block";
-                            document.getElementById('error').style.display = "none";
-                            document.getElementById('success').style.display = "none";
-                            document.getElementById('empty').style.display = "none";
+                        const re =
+                            /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; //REGEX
+                        if (re.test(inputValue) === true) {
+                            document.getElementById("added").style.display = "block";
+                            document.getElementById("error").style.display = "none";
+                            document.getElementById("success").style.display = "none";
+                            document.getElementById("empty").style.display = "none";
+
                             setEmails([...emails, inputValue]);
                             setInputValue("");
+                        } else if (inputValue === "") {
+                            document.getElementById("empty").style.display = "block";
+                            document.getElementById("added").style.display = "none";
+                            document.getElementById("success").style.display = "none";
+                            document.getElementById("error").style.display = "none";
+                        } else if (re.test(inputValue) === false) {
+                            document.getElementById("error").style.display = "block";
+                            document.getElementById("added").style.display = "none";
+                            document.getElementById("empty").style.display = "none";
+                            document.getElementById("success").style.display = "none";
                         }
-                        else if ( (inputValue === "")){
-                            document.getElementById('empty').style.display = "block";
-                            document.getElementById('added').style.display = "none";
-                            document.getElementById('success').style.display = "none";
-                            document.getElementById('error').style.display = "none";
-                        }
-                        else if ( (re.test(inputValue) === false)){
-                            document.getElementById('error').style.display = "block";
-                            document.getElementById('added').style.display = "none";
-                            document.getElementById('empty').style.display = "none";
-                            document.getElementById('success').style.display = "none";
-                        }
-                        
                     }
                 }}
                 onChange={e => {
                     setInputValue(e.target.value);
                 }}
             />
-            <div className="sendButton">
-            <button class="main_btn" onClick={e => { 
-                if (emails.length === 0){
-                    document.getElementById('empty').style.display = "block";
-                    document.getElementById('success').style.display = "none";
-                    document.getElementById('added').style.display = "none";
-                    document.getElementById('error').style.display = "none";
-                }
-                else if (emails.length!==0){
-                document.getElementById('empty').style.display = "none";
-                document.getElementById('added').style.display = "none";
-                document.getElementById('error').style.display = "none";
-                document.getElementById('success').style.display = "block";}}
-            }
-            >
-                    <h2>SEND</h2>
-            </button>
         </div>
-        </div>
-        
     );
 };
--- React/src/Components/Main.js
+++ React/src/Components/Main.js
@@ -1,21 +1,26 @@
-import emailimg from "../Images/mail-removebg-preview.png";
+// import emailimg from "../Images/mail-removebg-preview.png";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 import { InputEmail } from "./Inputs/InputEmail";
-import Popup from './Core/Popup'
-import StyleText from './Core/Styletext'
+import Popup from "./Core/Popup";
+
 function Main(props) {
-    
+    // Form Data
+    const [emails, setEmails] = useState([]);
+
     return (
         <div className="main">
             <h1>Email Form</h1>
             <h3>Send breathtaking professional emails in one click!</h3>
             <h6>Please fill in the following inputs</h6>
             <Popup />
-            
-            <InputEmail />
-            <img className="email-img" src={emailimg} alt="Emailimg" />
-            <div class="textarea">
+
+            <InputEmail emails={emails} setEmails={setEmails} />
+
+            {/* <img className="email-img" src={emailimg} alt="Emailimg" /> */}
+            {/* <div class="textarea">
                 <StyleText></StyleText>
-            <textarea
+                <textarea
                     class="bodytext"
                     name="bio"
                     rows="10"
@@ -23,8 +28,29 @@ function Main(props) {
                     required
                     placeholder="    Body Paragraph..."
                 ></textarea>
-                
+            </div> */}
+            <div className="sendButton">
+                <Button
+                    variant="contained"
+                    size="large"
+                    onClick={e => {
+                        if (emails.length === 0) {
+                            document.getElementById("empty").style.display = "block";
+                            document.getElementById("success").style.display = "none";
+                            document.getElementById("added").style.display = "none";
+                            document.getElementById("error").style.display = "none";
+                        } else if (emails.length !== 0) {
+                            document.getElementById("empty").style.display = "none";
+                            document.getElementById("added").style.display = "none";
+                            document.getElementById("error").style.display = "none";
+                            document.getElementById("success").style.display = "block";
+                        }
+                    }}
+                >
+                    SEND
+                </Button>
             </div>
         </div>
     );
+}
 export default Main;
